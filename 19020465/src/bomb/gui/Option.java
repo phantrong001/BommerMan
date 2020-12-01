7c1b74da051d1d1c21147bb20fae08a8944dd310
phantrong001 <phantrong001@gmail.com>
2020-12-01 15:37:39 +0700

    first commit

---
+++ 19020465/src/bomb/gui/Option.java
@@ -0,0 +1,43 @@
+package bomb.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Option extends JPanel implements ActionListener{
+	private MyContainer mContainer;
+	private JLabel lbbackground;
+	private ImageIcon backgroundIcon;
+	private JButton btn_ok;
+	
+	public Option(MyContainer mContainer) {
+		this.mContainer = mContainer;
+		setBackground(Color.YELLOW);
+		setLayout(null);
+		initCompts();
+	}
+	
+	public void initCompts(){
+		lbbackground = new JLabel();
+		lbbackground.setBounds(0, 0, GUI.WIDTHJF, GUI.HEIGHTJF);
+		lbbackground.setBackground(Color.BLACK);
+		backgroundIcon = new ImageIcon(getClass().getResource("/Images/background_option.png"));
+		lbbackground.setIcon(backgroundIcon);
+		add(lbbackground);
+		
+		btn_ok = new JButton();
+		btn_ok.setText("OK");
+		btn_ok.setBounds(400, 520, 100, 50);
+		btn_ok.addActionListener(this);
+		add(btn_ok);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btn_ok){
+			mContainer.setShowMenu();
+		}	
+	}
+	
+}
